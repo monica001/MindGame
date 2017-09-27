20ecb6cd31248f3b9f12f621f0188113d1ee6fe7
monica001 <rebvu2020@gmail.com>
2017-09-27 17:16:44 +0800

    DAL

---
+++ MindGame/DAL/QuizContext.cs
@@ -0,0 +1,29 @@
+﻿using MindGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MindGame.DAL
+{
+    public class QuizContext : DbContext
+    {
+        public QuizContext() : base("MindGame")
+        {
+        }
+
+        public DbSet<Quiz> Quizs { get; set; }
+        public DbSet<QuizAnalyze> QuizAnalyzes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // modelBuilder.Entity<Quiz>().HasMany(x => x.QuizAnalyze);
+            modelBuilder.Entity<Quiz>().HasMany(x => x.QuizAnalyze).
+                 WithRequired(x => x.Quiz).HasForeignKey(x=>x.QuizID);
+        }
+    }
+}
